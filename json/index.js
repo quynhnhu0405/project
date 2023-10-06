54f15ae706ca7ebf7091fa179444b545265ecbf4
Perry Hoang <hoanghuynh5555@gmail.com>
2023-10-06 16:14:08 +0700

    render category and product

---
+++ json/index.js
@@ -0,0 +1,112 @@
+const category = [
+  {
+    id: 1,
+    name: "Dining",
+    image:
+      "https://www.southernliving.com/thmb/IZxhy8uo1y6G2H8ONFJqx3_8CgE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/2577401_jacks_3791-1-51a1cd910a8d4a0099ebfa7760e9f488-257a0b98c4aa40ec84c0f059164194a0.jpg",
+  },
+  {
+    id: 2,
+    name: "Living",
+    image:
+      "https://media.architecturaldigest.com/photos/62f3c04c5489dd66d1d538b9/master/pass/_Hall_St_0256_v2.jpeg",
+  },
+  {
+    id: 3,
+    name: "Bedroom",
+    image:
+      "https://www.thespruce.com/thmb/HrWPmUEjB_yA71L6OJjiQPPvov4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/master-bedroom-in-new-luxury-home-with-chandelier-and-large-bank-of-windows-with-view-of-trees-1222623844-212940f4f89e4b69b6ce56fd968e9351.jpg",
+  },
+];
+
+const product = [
+  {
+    id: 1,
+    name: "Table Lamp",
+    image:
+      "https://zago-store.vn/wp-content/uploads/2021/03/lamp-mira012-2.jpg",
+    description: "A stylish table lamp for your room",
+    price: 25,
+    cost: 15,
+  },
+  {
+    id: 2,
+    name: "Wall Clock",
+    image:
+      "https://img.koalaliving.com.au/products/128126/4faef007-7b54-45cc-a089-b7909a7edfd7.png",
+    description: "A decorative wall clock",
+    price: 20,
+    cost: 12,
+  },
+  {
+    id: 3,
+    name: "Candle Holder",
+    image:
+      "https://bloomr.ae/cdn/shop/products/bloomr-marble-decor-candle-holder-artificial-flowers-artificial-trees-artificial-plants-dubai-saudi-arabia-uae-34420498170018_2024x2024.jpg?v=1670133846",
+    description: "A beautiful candle holder",
+    price: 12,
+    cost: 8,
+  },
+  {
+    id: 4,
+    name: "Vase",
+    image:
+      "https://cb2.scene7.com/is/image/CB2/ValoWhiteVaseSHF22/$web_pdp_main_carousel_sm$/220524125913/valo-matte-white-vase.jpg",
+    description: "An elegant vase for your flowers",
+    price: 18,
+    cost: 10,
+  },
+  {
+    id: 5,
+    name: "Photo Frame",
+    image:
+      "https://wallmantra.com/cdn/shop/products/wallmantra-photo-frame-family-hanging-photo-frame-walnut-32598631415974_1024x1024.jpg?v=1645263836",
+    description: "A lovely photo frame",
+    price: 10,
+    cost: 6,
+  },
+  {
+    id: 6,
+    name: "Cushion Cover",
+    image:
+      "https://www.chumbak.com/cdn/shop/products/8907605117886_1.jpg?v=1673504086&width=2048",
+    description: "A soft cushion cover",
+    price: 8,
+    cost: 4,
+  },
+  {
+    id: 7,
+    name: "Curtains",
+    image:
+      "https://www.ikea.com/au/en/images/products/silverloenn-sheer-curtains-1-pair-white__1209501_pe909266_s5.jpg",
+    description: "Beautiful curtains for your windows",
+    price: 30,
+    cost: 20,
+  },
+  {
+    id: 8,
+    name: "Wall Art",
+    image: "https://i.ebayimg.com/images/g/1qcAAOSwRkhiO3mc/s-l1600.jpg",
+    description: "Artistic wall art for your room",
+    price: 22,
+    cost: 14,
+  },
+  {
+    id: 9,
+    name: "Rug",
+    image:
+      "https://www.thespruce.com/thmb/USB8zpcUA1uJ6IOhhv0X7vQ-DzU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1342077578-d6e847fc39ce4861a71bdf933567fac0.jpg",
+    description: "A cozy rug for your floor",
+    price: 35,
+    cost: 25,
+  },
+  {
+    id: 10,
+    name: "Throw Blanket",
+    image:
+      "https://i5.walmartimages.com/seo/Better-Homes-Gardens-Faux-Fur-Throw-Blanket-50-x-60-Snow-Leopard-White_ab518b4b-59e5-4280-9b59-b9654c5c03d1.c74725f91cd1587bd01567b78b0afb82.jpeg",
+    description: "A warm throw blanket",
+    price: 28,
+    cost: 18,
+  },
+];
